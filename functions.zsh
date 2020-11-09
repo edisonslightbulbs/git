a405e85b8b11db19c94c85cd9364cd4813a27ead
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2020-11-09 18:42:19 +0100

    cleaning redundant code fixing subrm function

--- functions.zsh
+++ functions.zsh
@@ -48,16 +48,11 @@ function attach() {
 }
 
 # sublst:  [ tested ]
-#   Lists current repositorys' submodules.
+#   Lists current repository submodules.
 function sublst() {
     git config --file .gitmodules --get-regexp path | awk '{ print $2 }'
 }
 
-# rbm: [ tested ]
-#    Rebase calling branch or current master or main.
-function rbm() {
-    git rebase master || git rebase main
-}
 
 # clone: [ tested ]
 #    For a repo with submodules, clone recursively
@@ -67,7 +62,7 @@ function clone() {
 }
 
 # diff: [ tested ]
-#   Always shows a reletive, most recent diff, i.e., shows
+#   Always shows a relative, most recent diff, i.e., shows
 #     : diff iff un-staged, else -> diff iff staged, else -> diff iff committed
 function diff() {
     if ! git diff-files --quiet --ignore-submodules --; then
@@ -113,47 +108,49 @@ function root() {
 # sr: [ tested ]
 #   Removes a submodule, correctly.
 function subrm() {
-    local ROOT
-    ROOT="$PWD"
+    shift
+    echo "$1"
+     local ROOT
+     ROOT="$PWD"
 
-    #  find the absolute path of a submodule iff it exists:
-    #   to allow for non-alphanumeric filenames change from using [ -print0 ] and [ -0 ]
-    #   while read [ -r ] prevents mangling of back slashes
-    find . -type d -name "$1" -print0 | xargs -0 realpath | while read -r MODULEPATH; do
-    cd "$MODULEPATH" || return
+     #  find the absolute path of a submodule iff it exists:
+     #   to allow for non-alphanumeric filenames change from using [ -print0 ] and [ -0 ]
+     #   while read [ -r ] prevents mangling of back slashes
+     find . -type d -name "$1" -print0 | xargs -0 realpath | while read -r MODULEPATH; do
+     cd "$MODULEPATH" || return
 
-        # use the .git *file to ID the submodule
-        if test -f '.git'; then
-            # todo: optimize find strategy by grepping the .git file
-            echo "Found submodule: $1"
-            echo "Removing submodule: $1"
+         # use the .git *file to ID the submodule
+         if test -f '.git'; then
+             # todo: optimize find strategy by grepping the .git file
+             echo "Found submodule: $1"
+             echo "Removing submodule: $1"
 
-            # strip-off leading absolute path
-            MODULE=${MODULEPATH#"$ROOT"/}
-            echo "$MODULE"
+             # strip-off leading absolute path
+             MODULE=${MODULEPATH#"$ROOT"/}
+             echo "$MODULE"
 
-            cd "$ROOT" || return
+             cd "$ROOT" || return
 
-            # correctly remove git submodule
-            # i. deregister submodule path
-            if ! git submodule deinit -f -- "$MODULE"; then
-                echo "Something went wrong: unable to unregister submodule path"
-            else
-                # ii. remove submodule
-                if ! git rm -f "$MODULE"; then
-                    echo "Something went wrong: unable to remove submodule from git repository"
-                else
-                    # iii. remove submodule from .git/modules
-                    if ! rm -rf .git/modules/"$MODULE"; then
-                        echo "Something went wrong: unable delete submodule from git module path"
-                    else
-                        echo "Submodule removed from project repository successfully"
-                        return
-                    fi
-                fi
-            fi
-        fi
-        done
+             # correctly remove git submodule
+             # i. deregister submodule path
+             if ! git submodule deinit -f -- "$MODULE"; then
+                 echo "Something went wrong: unable to unregister submodule path"
+             else
+                 # ii. remove submodule
+                 if ! git rm -f "$MODULE"; then
+                     echo "Something went wrong: unable to remove submodule from git repository"
+                 else
+                     # iii. remove submodule from .git/modules
+                     if ! rm -rf .git/modules/"$MODULE"; then
+                         echo "Something went wrong: unable delete submodule from git module path"
+                     else
+                         echo "Submodule removed from project repository successfully"
+                         return
+                     fi
+                 fi
+             fi
+         fi
+         done
 }
 
 # # state():
