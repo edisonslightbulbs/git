951792f314afeb72386aba6f0b0abcda9b5ab7b0
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-07-05 20:22:22 +0200

    code review

--- submodule.zsh
+++ submodule.zsh
@@ -76,48 +76,10 @@ function substat() {
 # subrm
 #   Removes a submodule the right way.
 function subrm() {
-    shift
-    echo "$1"
-    local ROOT
-    ROOT="$PWD"
-
-    #  In finding the absolute path of a submodule,
-    #  allow for non-alphanumeric filenames using
-    #  [ -print0 ] and [ -0 ]. Also, use [ -r ] to
-    #  prevents mangling of back slashes
-    find . -type d -name "$1" -print0 | xargs -0 realpath | while read -r MODULEPATH; do
-    cd "$MODULEPATH" || return
-
-        # use the .git *file to ID the submodule
-        if test -f '.git'; then
-            # todo: optimize find strategy by grepping the .git file
-            echo "Found submodule: $1"
-            echo "Removing submodule: $1"
-
-            # strip-off leading absolute path
-            MODULE=${MODULEPATH#"$ROOT"/}
-            echo "$MODULE"
-
-            cd "$ROOT" || return
-
-            # correctly remove git submodule
-            # i) deregister submodule path
-            if ! git submodule deinit -f -- "$MODULE"; then
-                echo "Something went wrong: unable to unregister submodule path"
-            else
-                # ii) remove submodule
-                if ! git rm -f "$MODULE"; then
-                    echo "Something went wrong: unable to remove submodule from git repository"
-                else
-                    # iii) remove submodule from .git/modules
-                    if ! rm -rf .git/modules/"$MODULE"; then
-                        echo "Something went wrong: unable delete submodule from git module path"
-                    else
-                        echo "Submodule removed from project repository successfully"
-                        return
-                    fi
-                fi
-            fi
-        fi
-    done
+    git submodule deinit -f "$@"
+    git rm -rf "$@"
+    # Note: asubmodule (no trailing slash)
+    # or, if you want to leave it in your working tree
+    # git rm --cached <asubmodule>
+    # rm -rf .git/modules/<asubmodule>
 }
